91856c9a1a14a86f69598f66420291c6dc02e7c0
William Payne <payne.j.will@gmail.com>
2021-01-13 09:16:10 -0600

    fix

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -77,46 +77,42 @@ export default function LoginForm({ isLoginOpen, handleCloseModal }) {
 
   //get Context and history
 
-  
   const { setUserData } = React.useContext(UserContext);
 
   //functions to handle sumbit for login
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      const loginUser = { email, password };
+      const loginRes = await axios.post("/users/login", loginUser, authToken);
 
-
-
-
-
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        const loginUser = { email, password };
-        const loginRes = await axios.post("/users/login", loginUser, authToken);
-
-        const tokenRes = await axios.post(
-          "/users/tokenIsValid",
-          null,
-          { headers: { "x-auth-token": token } }
-        );
-        if (tokenRes.data) {
-          const userRes = await axios.get("/users", {
-            headers: { "x-auth-token": token },
-          });
-        }
-        
-        setUserData({
-          token: loginRes.data.token,
-          user: loginRes.data.user,
-        });
-        localStorage.setItem("auth-token", loginRes.data.token);
-        history.push("/profile");
-      } catch (err) {
-        err.response.data.msg && setError(err.response.data.msg);
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = null;
       }
-    };
+
+      const tokenRes = await axios.post("/users/tokenIsValid", null, {
+        headers: { "x-auth-token": token },
+      });
+      if (tokenRes.data) {
+        const userRes = await axios.get("/users", {
+          headers: { "x-auth-token": token },
+        });
+      }
+
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/profile");
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
+  };
 
   const handleRegisterOpen = () => {
     registerIsOpen(true);
